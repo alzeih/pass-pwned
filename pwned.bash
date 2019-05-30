e20a351bdf08845bf732cfb6257b7607fbd9c364
alzeih <alzeih@users.noreply.github.com>
2019-05-30 17:40:52 +1200

    die with error if format not as expected instead of issuing a warning
    don't need [[ -z "$foo" ]], use [ -z "$foo" ]
    make newline trim both \r and \n
    
    Signed-off-by: alzeih <alzeih@users.noreply.github.com>

--- pwned.bash
+++ pwned.bash
@@ -9,40 +9,40 @@ USER_AGENT="Password-Store-bash-extension-pass-pwned"
 
 cmd_pwned() {
 
-  contents=$($GPG -d "${GPG_OPTS[@]}" "$passfile" | head -n +1 | tr -d '\r')
-  if [[ -z "$contents" ]]; then
+  contents=$($GPG -d "${GPG_OPTS[@]}" "$passfile" | head -n +1 | tr -d '\r\n')
+  if [ -z "$contents" ]; then
     die "Failed to read password"
   fi
 
   sha1=$(echo -n "$contents" | sha1sum | cut -d ' ' -f 1 | tr -d '\r')
-  if [[ -z "$sha1" ]]; then
+  if [ -z "$sha1" ]; then
     die "Failed to generate sha1"
   fi
 
   sha1prefix="${sha1:0:5}"
-  if [[ -z "$sha1prefix" ]]; then
+  if [ -z "$sha1prefix" ]; then
     die "Failed to get sha1 prefix"
   fi
 
   sha1suffix="${sha1:5}"
-  if [[ -z "$sha1suffix" ]]; then
+  if [ -z "$sha1suffix" ]; then
     die "Failed to get sha1 suffix"
   fi
 
   result=$(curl --fail --silent -A "$USER_AGENT" "https://api.pwnedpasswords.com/range/$sha1prefix")
-  if [[ -z "$result" ]]; then
+  if [ -z "$result" ]; then
     die "Failed to read from API"
   fi
 
-  match=$(echo "$result" | grep -i "$sha1suffix" | tr -d '\r')
-  if [[ -z "$match" ]]; then
+  match=$(echo "$result" | grep -i "$sha1suffix" | tr -d '\r\n')
+  if [ -z "$match" ]; then
     echo "Password not found in haveibeenpwned"
     exit 0
   fi
 
-  count=$(echo -n "$match" | cut -s -d ':' -f 2 | tr -d '\r')
-  if [[ -z "$count" ]]; then
-    echo "Warning: can't parse output. Count may be incorrect."
+  count=$(echo -n "$match" | cut -s -d ':' -f 2 | tr -d '\r\n')
+  if [ -z "$count" ]; then
+    die "Cannot parse output from API"
   fi
 
   echo "Password found in haveibeenpwned $count times"
@@ -53,9 +53,9 @@ path="$1"
 passfile="$PREFIX/$path.gpg"
 check_sneaky_paths "$path"
 
-if [[ -f $passfile ]]; then
+if [ -f "$passfile" ]; then
   cmd_pwned "$@"
-elif [[ -z $path ]]; then
+elif [ -n "$path" ]; then
   die "Usage: $PROGRAM $COMMAND pass-name"
 else
   die "Error: $path is not in the password store."
